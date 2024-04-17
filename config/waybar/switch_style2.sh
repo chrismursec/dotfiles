269e99b0414e89b0a82d326d02bbaa4d6d62a57e
chrismursec <chrisamursec@gmail.com>
2024-04-17 16:43:38 +0100

    new waybar

--- config/waybar/switch_style2.sh
+++ config/waybar/switch_style2.sh
@@ -2,5 +2,4 @@
 killall waybar
 SDIR="$HOME/.config/waybar"
 waybar -c "$SDIR"/config2 -s "$SDIR"/style2.css &
-swaymsg gaps outer all set 0 && swaymsg gaps inner all set 20
-
+swaymsg gaps outer all set 10 && swaymsg gaps inner all set 10
